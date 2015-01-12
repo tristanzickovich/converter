24bf8cb604571c5384e9420717478af9d3491dc9
Tristan Zickovich <tzick002@well.cs.ucr.edu>
2015-01-11 22:19:46 -0800

    Added Dec to Binary Algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
-
+#include <stack>
 using namespace std;
 
+void convertBinary(int val){
+	stack<int> bstring;
+	cout << val << " in binary is: ";
+	int count = 0;
+	while(val > 0){
+		bstring.push(val % 2);
+		val /= 2; 
+		++count;
+		if(count == 4)
+			count = 0;
+	}
+	while(count != 0 && count < 4){
+		bstring.push(0);
+		++count;
+	}
+	count = 0;
+	while(!bstring.empty()){
+		cout << bstring.top();
+		bstring.pop();
+		++count;
+		if(count == 4){
+			cout << ' ';
+			count = 0;
+		}
+	}
+	cout << endl;
+}
+
 int main(){
+	int input, start;
+     cout << "Enter a Decimal number to be converted to Binary: ";
+     cin >> input;
+	convertBinary(input);
      return 0;
 }
